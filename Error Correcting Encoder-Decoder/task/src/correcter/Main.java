4f43c4abf312b5369d08d7298a5272cb7cf514c8
Kamil <kamilnowak911@gmail.com>
2020-10-07 01:02:13 +0200

    added encoder class

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -10,8 +10,9 @@ public class Main {
         TextHarmer textHarmer = new TextHarmer();
         Encoder encoder = new Encoder();
         String input = new Input().getUserInput();
+        String encodedInput = encoder.encode(input);
         System.out.println(input);
-        System.out.println(encoder.encode(input));
-        System.out.println(textHarmer.broke(input));
+        System.out.println(encodedInput);
+        System.out.println(textHarmer.broke(encodedInput));
     }
 }
